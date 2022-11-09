d5bc8d27068a6c1947e835c8e257f13fa636bcb8
lucia15 <lucia15.maga88@gmail.com>
2022-11-09 03:11:40 -0300

    Improve some auxiliary functions, add more documentation.

--- utils.py
+++ utils.py
@@ -11,8 +11,10 @@ def csvs_to_df(csvs=csvs, desired_col=desired_col):
     '''
     Load csvs to pandas dataframe. Iterates over chunks to save memory use.
     
-    :csvs: list with the path to the csvs files
-    :desired_col: columns we are interested in
+    :csvs: list with the path to the csvs files.
+    :desired_col: columns we are interested in.
+    
+    :returns: a pandas dataframe with the data in the csv files.
     '''
     
     df = pd.DataFrame()
@@ -38,8 +40,15 @@ def csvs_to_df(csvs=csvs, desired_col=desired_col):
 
 
 def process_df(df):
+    '''
+    Convert date and time in string format to a DateTime object.
     
-    df['timestamp'] = pd.to_datetime(df['time'])
+    :df: pandas dataframe to modify.
+    
+    :returns: modified pandas dataframe.
+    '''
+    
+    df['timestamp'] = pd.to_datetime(df['time']) 
     df.drop('time', axis=1, inplace=True)
     
     df.sort_values(by='timestamp', inplace=True, ascending=True) # sort by date
@@ -52,10 +61,12 @@ def process_df(df):
 
 def move_col_to_first_pos(df, col_name):
     '''
-    shift column 'col_name' to first position
+    shift column 'col_name' to first column position.
     
-    :df:
-    :col_name:
+    :df: pandas dataframe to modify.
+    :col_name: column that will be moved to first column position.
+    
+    :returns: modified pandas dataframe.
     '''
     
     first_column = df.pop(col_name)
@@ -69,9 +80,10 @@ def string_array_to_array(response_array, m=25):
     Convert string with response array into a list of responses. 
     Convert new list elements (which are strings) into integers.
     
-    :response_array: string with response array
-    :m: int with response array length (default to 25)
-    :returns: list with response array formated
+    :response_array: string with response array.
+    :m: int with response array length (default to 25).
+    
+    :returns: list with response array formated.
     '''
     
     if type(response_array) == float: # they are all NaN or none of them are
@@ -85,7 +97,12 @@ def string_array_to_array(response_array, m=25):
 
 def process_list_elem(elem):
     '''
-    :elem: list element (string)
+    Delete brackets and quotations from elem (string).
+    Convert elem to integer if possible.
+    
+    :elem: list element (string).
+    
+    :returns: new list element
     '''
     
     try:
@@ -96,43 +113,62 @@ def process_list_elem(elem):
     return new_elem
 
 
-def get_df_assess(df, desired_col=assess_desired_col):
+def get_df_assess(df, desired_col=assess_desired_col, new_cols=var_names, new_cols2=scores):
     '''
-    :df: pandas dataframe
-    '''
-    df_assess = df[assess_desired_col].copy()
+    Process previous dataframe (with all the data from the raw csv files) to
+    create new columns by spliting the answers, that are all together, in the columns
+    'question_scores' and 'category_scores'.
+    Create also new columns from these new columns.
     
+    
+    :df: pandas dataframe with all the data from the raw csv files.
+    :desired_col: columns we are interested in.
+    :new_cols: list with new columns names (for 'question_scores' answers).
+    :new_cols2: list with new columns names (for 'category_scores' answers).
+    
+    :returns: a pandas dataframe with a column for each type of answer.
+    '''
+    df_assess = df[assess_desired_col].copy() #filter desired columns
+    
+    # filter only complete assessments
     df_assess = df_assess[df_assess['event']=='Complete Assessment']
     df_assess.drop('event', axis=1, inplace=True)
     
     
+    # first convert the data on each column, from a string containing all the answers to a list of answers
+    
     df_assess['question_scores'] = df_assess['question_scores'].apply(lambda x: string_array_to_array(x))
-    df_assess['question_labels'] = df_assess['question_labels'].apply(lambda x: string_array_to_array(x)) 
-                                                      
+    df_assess['question_labels'] = df_assess['question_labels'].apply(lambda x: string_array_to_array(x))  
+    
     df_assess['category_scores'] = df_assess['category_scores'].apply(lambda x: string_array_to_array(x, m=3))
     df_assess['categories'] = df_assess['categories'].apply(lambda x: string_array_to_array(x, m=3))
     
-
+    
+    # new columns from 'question_scores' answers
     df_assess[var_names] = pd.DataFrame(df_assess['question_scores'].tolist(), index=df_assess.index)
     
-    
-    col_names = df_assess.columns
-    
-    df_assess = df_assess.assign(phq_gad_score = df_assess[['phq_gad_1', 'phq_gad_2']].mean(axis=1)*2)
-    df_assess = df_assess.assign(phq_dep_score = df_assess[['phq_dep_1', 'phq_dep_2']].mean(axis=1)*2)
-    df_assess = df_assess.assign(phq_mood_score = df_assess.phq_gad_score + df_assess.phq_dep_score)
-    df_assess = df_assess.assign(wemwbs_score = df_assess[col_names[14:28]].mean(axis=1)*14)
-    df_assess = df_assess.assign(wsas_score = df_assess[col_names[29:34]].mean(axis=1)*5)
-    
- 
+    # new columns from 'category_scores' answers
     df_assess[scores] = pd.DataFrame(df_assess['category_scores'].tolist(), index=df_assess.index)
     
     df_assess['gs_mood_score'] = reverse(df_assess.gs_mood_score, 12)
     df_assess['gs_wsas_score'] = reverse(df_assess.gs_wsas_score, 40)
-    df_assess['phq_mood_score'] = reverse(df_assess.phq_mood_score, 12)
+    
+    # more new columns
+    df_assess = df_assess.assign(phq_gad_score = df_assess[['phq_gad_1', 'phq_gad_2']].mean(axis=1)*2)
+    df_assess = df_assess.assign(phq_dep_score = df_assess[['phq_dep_1', 'phq_dep_2']].mean(axis=1)*2)
+    df_assess = df_assess.assign(phq_mood_score = df_assess.phq_gad_score + df_assess.phq_dep_score)
+
     df_assess['phq_dep_score'] = reverse(df_assess.phq_dep_score, 12)
     df_assess['phq_gad_score'] = reverse(df_assess.phq_gad_score, 12)
+    df_assess['phq_mood_score'] = reverse(df_assess.phq_mood_score, 12)
+    
+    col_names = df_assess.columns
+    
+    df_assess = df_assess.assign(wemwbs_score = df_assess[col_names[14:28]].mean(axis=1)*14)
+    df_assess = df_assess.assign(wsas_score = df_assess[col_names[29:34]].mean(axis=1)*5)    
+    
     df_assess['wsas_score'] = reverse(df_assess.wsas_score, 40)
+    
     
     df_assess.sort_values(by=['distinct_id','timestamp'], inplace=True, ascending=True) # sort by ID and date
     
@@ -143,18 +179,22 @@ def get_df_assess(df, desired_col=assess_desired_col):
 
 def reverse (series, const):
     '''
-    :series: pandas dataframe column
-    :const: int
+    Substract each column value from constant.
+    
+    :series: pandas dataframe column.
+    :const: constant value (int).
+    
+    :returns: column with new values.
     '''
     return const - series
 
 
 def save_pd_to_csv(df, file_name):
     '''
-    Save dataframe to csv file
+    Save dataframe to csv file.
     
-    :df:
-    :file_name:
+    :df: pandas dataframe you want to save.
+    :file_name: name of the csv file.
     '''
     path = os.path.join('csvs/processed', file_name)
     
@@ -162,6 +202,13 @@ def save_pd_to_csv(df, file_name):
     
     
 def df_from_csv(file_name):
+    '''
+    Load csv file into a pandas dataframe.
+    
+    :file_name: name of the csv file.
+    
+    :returns: a pandas dataframe with the data in the csv file.
+    '''
     
     folder = 'csvs/processed'
     path = os.path.join(folder, file_name)
@@ -171,24 +218,34 @@ def df_from_csv(file_name):
     
 def get_first_last_assess(df_assess):
     '''
-    Get first assessments, last assessments, and first and last for each uuid on a single line
+    Get first assessments, last assessments, and first and last for each uuid on a single line.
+    
+    :df_assess: previous dataframe
+    
+    :returns: 
     '''
     
     df_assess_first = df_assess.groupby('distinct_id', as_index=False).first()
     df_assess_last = df_assess.groupby('distinct_id', as_index=False).last()
+    
+    df_assess_first = df_assess_first.add_suffix('_first')
     df_assess_last = df_assess_last.add_suffix('_last')
+    
     df_assess_fl = df_assess_first.join(df_assess_last)
+    
+    df_assess_fl = df_assess_fl.rename(columns={'distinct_id_first': 'distinct_id'})
+    df_assess_fl = df_assess_fl.drop('distinct_id_last', axis=1)
 
     # add a week index
     # df_assess_first['week_index'] = ((df_assess_first.timestamp - dt.datetime(2021, 1, 1)).dt.days / 7).astype(int)
     # df_week = df_assess_first.groupby(df_assess_first.week_index).mean()
     
-    df_assess_fl['total_time'] = (df_assess_fl.timestamp_last - df_assess_fl.timestamp).dt.days
-    df_assess_fl['dep_diff'] = (df_assess_fl.phq_dep_score_last - df_assess_fl.phq_dep_score)
-    df_assess_fl['anx_diff'] = (df_assess_fl.phq_gad_score_last - df_assess_fl.phq_gad_score)
-    df_assess_fl['mood_diff'] = (df_assess_fl.phq_mood_score_last - df_assess_fl.phq_mood_score)
-    df_assess_fl['func_diff'] = (df_assess_fl.wsas_score_last - df_assess_fl.wsas_score)
-    df_assess_fl['wb_diff'] = (df_assess_fl.wemwbs_score_last - df_assess_fl.wemwbs_score)
+    df_assess_fl['total_time'] = (df_assess_fl.timestamp_last - df_assess_fl.timestamp_first).dt.days
+    df_assess_fl['dep_diff'] = (df_assess_fl.phq_dep_score_last - df_assess_fl.phq_dep_score_first)
+    df_assess_fl['anx_diff'] = (df_assess_fl.phq_gad_score_last - df_assess_fl.phq_gad_score_first)
+    df_assess_fl['mood_diff'] = (df_assess_fl.phq_mood_score_last - df_assess_fl.phq_mood_score_first)
+    df_assess_fl['func_diff'] = (df_assess_fl.wsas_score_last - df_assess_fl.wsas_score_first)
+    df_assess_fl['wb_diff'] = (df_assess_fl.wemwbs_score_last - df_assess_fl.wemwbs_score_first)
 
     dep_sd = df_assess_fl['dep_diff'].std()
     anx_sd = df_assess_fl['anx_diff'].std()
@@ -206,6 +263,12 @@ def get_first_last_assess(df_assess):
 
 
 def get_df_use_resource(df):
+    '''
+    
+    :df:
+    
+    :returns: 
+    '''
     
     filter1 = (df['event'] == 'Access Resource')
     filter2 = ((df['event'] == 'Page Load') & (df['url_name'] == 'resource-wrapper'))
@@ -214,6 +277,12 @@ def get_df_use_resource(df):
 
 
 def get_df_accesses(df_use_resource):
+    '''
+    
+    :df_use_resource:
+    
+    :returns: 
+    '''
     
     #Groupby user and get the timestamps for each user
     timestamps_by_user = df_use_resource.groupby(['distinct_id']).timestamp
@@ -249,9 +318,11 @@ def get_df_accesses(df_use_resource):
 
 def get_df_first_access(df_accesses):
     '''
-    Compute the first non-null entry of each column.
+    Compute the first non-null entry of each column
     
     :df_accesses:
+    
+    :returns: 
     '''
 
     df_first_access = df_accesses.groupby('distinct_id').first()
@@ -263,6 +334,16 @@ def get_df_first_access(df_accesses):
 
 
 def get_grouped_event(df, event_type, id_str, function=None, rename=None):
+    '''
+    
+    :df:
+    :event_type:
+    :id_str:
+    :function:
+    :rename:
+    
+    :returns: 
+    '''
     
     df = df[df['event'] == event_type].groupby(id_str, as_index=False) # select event type and group
     
@@ -280,7 +361,10 @@ def get_df_first_access_merged(df, df_accesses, df_first_access):
     Users who took assessments and accessed resources.
     
     :df:
+    :df_accesses:
     :df_first_access:
+    
+    :returns: 
     '''
     
     df0 = get_grouped_event(df, 
@@ -335,6 +419,12 @@ def get_df_first_access_merged(df, df_accesses, df_first_access):
 
 
 def get_df_access_count(df_accesses):
+    '''
+    
+    :df_accesses:
+    
+    :return:
+    '''
     
     df_access_count = df_accesses.groupby('distinct_id', as_index=False).count()[['distinct_id', 'event']]
     df_access_count.rename(columns = {'event': 'access_count'}, inplace=True)
@@ -343,6 +433,13 @@ def get_df_access_count(df_accesses):
 
 
 def get_df_assess_fl_access(df_assess_fl, df_access_count):
+    '''
+    
+    :df_assess_fl:
+    :df_access_count:
+    
+    :return:
+    '''
 
     df_assess_fl_access = df_assess_fl.merge(df_access_count, on='distinct_id', how='left')
     df_assess_fl_access['access_count'] = df_assess_fl_access['access_count'].fillna(0)
@@ -350,7 +447,18 @@ def get_df_assess_fl_access(df_assess_fl, df_access_count):
     return df_assess_fl_access
        
     
-def get_results_table (df, headers, score_vars, score_labels, std=True, d=False):
+def get_results_table(df, headers, score_vars, score_labels, std=True, d=False):
+    '''
+    
+    :df:
+    :headers:
+    :score_vars:
+    :score_lavels:
+    :std: (bool)
+    :d: (bool)
+    
+    :return:
+    '''
     
     # check for correct number of labels problems
     if len(score_vars) != len(score_labels): 
@@ -387,22 +495,6 @@ def get_results_table (df, headers, score_vars, score_labels, std=True, d=False)
     
     
     
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # expects a list of lists, each list containing two dates to pass to API
 
